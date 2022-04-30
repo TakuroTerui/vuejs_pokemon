0f77130af361f18ba831b70cf2ede970836f1ba2
照井拓朗 <>
2022-04-30 15:44:12 +0900

    axios導入

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <div style="width: 700px; margin: auto; padding-top: 50px;">
+    <h3>掲示板に投稿する</h3>
+    <label for="name">ニックネーム</label>
+    <input
+      id="name"
+      type="text"
+      v-model="name"
+    >
+    <br>
+    <br>
+    <label for="comment">コメント</label>
+    <textarea id="comment" v-model="comment"></textarea>
+    <br>
+    <button @click="createComment">コメントをサーバーに送る</button>
+    <h2>掲示板</h2>
+
     <router-view name="header"></router-view>
     <transition
       name="fade"
@@ -13,11 +28,42 @@
 </template>
 
 <script>
+import axios from "axios";
 
 export default {
+  data() {
+    return {
+      name: "",
+      comment: ""
+    }
+  },
   methods: {
     beforeEnter() {
       this.$root.$emit('triggerScroll');
+    },
+    createComment() {
+      axios.post(
+        "http://127.0.0.1:8000/api/register/",
+        {
+          "status": {
+            stringValue: "public"
+          },
+          "title": {
+            stringValue: this.name
+          },
+          "body": {
+            stringValue: this.comment
+          },
+        }
+      )
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+      this.name = "";
+      this.comment = "";
     }
   }
 }
